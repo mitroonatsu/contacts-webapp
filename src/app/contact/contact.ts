a2d0815b112af8cf3f05b34fc9afcf839ecebfe3
mitroskii <mitro.onatsu@hotmail.com>
2018-05-16 14:32:35 +0300

    Added datas for contacts

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -2,10 +2,24 @@ export class Contact {
   id: number;
   firstName: string;
   lastName: string;
+  streetAddress: string;
+  city: string;
+  postalCode: string;
+  phoneNumber: string;
+  emailAddress: string;
 
-  constructor(id?: number, firstName?: string, lastName?: string) {
+
+  constructor(id?: number, firstName?: string, lastName?: string, streetAddress?: string, city?: string, postalCode?: string,
+              phoneNumber?: string, emailAddress?: string) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.postalCode = postalCode;
+    this.phoneNumber = phoneNumber;
+    this.emailAddress = emailAddress;
+
+
   }
 }
